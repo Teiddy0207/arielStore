064089700e284e8fc494b449eac781963df4319a
Ngô Anh <129403358+Teiddy0207@users.noreply.github.com>
2025-06-16 19:06:18 +0700

    update router

--- ArielStore/routes/web.php
+++ ArielStore/routes/web.php
@@ -31,4 +31,13 @@ Route::middleware(['employee.auth'])->group(function () {
     Route::patch('/employee/{employee}/toggle-active', [EmployeeController::class, 'toggleActive']);
 });
 
+use App\Http\Controllers\ProductController;
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::delete('/product-images/{productImage}', [ProductController::class, 'destroyImage'])->name('product-images.destroy');
 
